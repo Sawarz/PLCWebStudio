785c31edbaa5d4b7ee9600151372efbc11dd42b2
Sawarz <sawarz22@gmail.com>
2024-03-03 22:30:50 +0100

    feat: add wires

---
+++ src/components/PLCElements/Wire/Wire.tsx
@@ -0,0 +1,16 @@
+import styles from "./Wire.module.css";
+
+type Props = {
+	on: boolean | undefined;
+};
+
+export default function Wire({ on }: Props) {
+	return (
+		<div
+			style={{ backgroundColor: on ? "green" : "black" }}
+			className={styles.wire}
+		>
+			Wire
+		</div>
+	);
+}
