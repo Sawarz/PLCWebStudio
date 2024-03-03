dd9357bce7ef973cf0557934fbf9b4ad45cb8d0f
Sawarz <sawarz22@gmail.com>
2024-03-03 22:30:27 +0100

    feat: add wires

--- src/components/Network/Network.tsx
+++ src/components/Network/Network.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Contact from "../PLCElements/Contact/Contact";
 import Coil from "../PLCElements/Coil/Coil";
 import styles from "./Network.module.css";
+import Wire from "../PLCElements/Wire/Wire";
 
 type NetworkProps = {
 	id: string;
@@ -32,20 +33,28 @@ export default function Network(props: NetworkProps) {
 
 	return (
 		<div>
-			<div className={styles.divider}></div>
 			<div className={styles.elements}>
+				<Wire on={PLCData[0]?.on} />
 				{PLCData.map(({ element, on }, i) => {
 					if (element === "coil") {
 						return (
-							<Coil
-								on={on}
-								onClick={() => handleCoilClick(i)}
-							/>
+							<>
+								<Coil
+									on={on}
+									onClick={() => handleCoilClick(i)}
+								/>
+								<Wire on={PLCData[i]?.on} />
+							</>
 						);
 					}
 
 					if (element === "contact") {
-						return <Contact on={PLCData[i - 1].on} />;
+						return (
+							<>
+								<Contact on={PLCData[i - 1].on} />
+								<Wire on={PLCData[i - 1]?.on} />
+							</>
+						);
 					}
 				})}
 			</div>
