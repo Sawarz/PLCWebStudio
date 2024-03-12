0bb54f565965f0df90a290be1b94daff9dabeb34
Sawarz <sawarz22@gmail.com>
2024-03-12 22:07:14 +0100

    feat: Add Variables component and css reset

---
+++ src/components/Variables/Variables.tsx
@@ -0,0 +1,37 @@
+import { useVariableStore } from "../../stores/VariableStore";
+import styles from "./Variables.module.css";
+
+type Props = {};
+
+export default function Variables({}: Props) {
+	const { variables, addVariable } = useVariableStore((state: any) => state);
+
+	return (
+		<div className={styles.variables}>
+			<button
+				onClick={() => addVariable({ id: "1", name: "test", value: 0 })}
+				className={styles.addVariableButton}
+			>
+				Add Variable
+			</button>
+			{variables.map(
+				({
+					id,
+					name,
+					value,
+				}: {
+					id: string;
+					name: string;
+					value: number;
+				}) => {
+					return (
+						<div key={id}>
+							<p>{name}</p>
+							<p>{value}</p>
+						</div>
+					);
+				}
+			)}
+		</div>
+	);
+}
