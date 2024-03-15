519efb88109013e89c3fb5cf4974bdb59bad08b3
Sawarz <sawarz22@gmail.com>
2024-03-15 17:12:14 +0100

    feat: Create variable adding modal

---
+++ src/components/modals/VariableAddModal/VariableAddModal.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Type, useVariableStore } from "@/stores/VariableStore";
+import { v4 } from "uuid";
+import ValueInput from "@/components/ValueInput/ValueInput";
+import styles from "./VariableAddModal.module.css";
+
+type Props = {
+	closeModal: () => void;
+};
+
+export default function VariableAddModal({ closeModal }: Props) {
+	const { addVariable } = useVariableStore((state: any) => state);
+	const [name, setName] = useState<string>();
+	const [value, setValue] = useState<string>();
+	const [type, setType] = useState<Type>(Type.Boolean);
+
+	return (
+		<div className={styles.modal}>
+			<select onChange={(e) => setType(e.target.value as Type)}>
+				<option value={Type.Boolean}>Boolean</option>
+				<option value={Type.String}>String</option>
+				<option value={Type.Int}>Int</option>
+			</select>
+			<input
+				type='text'
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<ValueInput
+				type={type}
+				setValue={setValue}
+			/>
+			<button
+				onClick={() => {
+					addVariable({
+						id: v4(),
+						name,
+						value,
+						type,
+					});
+					closeModal();
+				}}
+			>
+				Add Variable
+			</button>
+			<button onClick={closeModal}>Cancel</button>
+		</div>
+	);
+}
