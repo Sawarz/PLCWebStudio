7ee5c110cad46b849e69e869b155715dc4a464a9
Sawarz <sawarz22@gmail.com>
2024-03-15 21:34:39 +0100

    feat: Add onclick event to coil

--- src/components/PLCElements/Coil/Coil.tsx
+++ src/components/PLCElements/Coil/Coil.tsx
@@ -1,14 +1,17 @@
+import { MouseEventHandler } from "react";
 import styles from "./Coil.module.css";
 
 type Props = {
 	on: boolean | undefined;
+	onClick: MouseEventHandler;
 };
 
-export default function Coil({ on }: Props) {
+export default function Coil({ on, onClick }: Props) {
 	return (
 		<div
 			style={{ borderColor: on ? "green" : "white" }}
 			className={styles.coil}
+			onClick={onClick}
 		/>
 	);
 }
