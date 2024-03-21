e296c80045511ff4d6f5fed81c27d22169031687
Sawarz <sawarz22@gmail.com>
2024-03-21 21:22:36 +0100

    feat: Add variable change through text input

--- src/components/PLCElements/ElementWrapper/ElementWrapper.tsx
+++ src/components/PLCElements/ElementWrapper/ElementWrapper.tsx
@@ -1,8 +1,9 @@
-import { MouseEventHandler, useState } from "react";
+import React, { MouseEventHandler, useState, useEffect } from "react";
 import { useNetworksStore } from "@/stores/NetworksStore";
 import ElementRenderer from "../ElementRenderer/ElementRenderer";
 import styles from "./ElementWrapper.module.css";
 import { ElementType } from "@/types/ElementEnum";
+import useDebounce from "@/util/useDebounce";
 
 type Props = {
 	id: string;
@@ -10,34 +11,73 @@ type Props = {
 };
 
 export default function ElementWrapper({ id, onClick }: Props) {
-	const [variable, setVariable] = useState();
-	const { networksData } = useNetworksStore((state: any) => state);
+	const [variable, setVariable] = useState("???");
+	const debouncedVariable = useDebounce(variable, 500);
+	const { networksData, modifyElement } = useNetworksStore(
+		(state: any) => state
+	);
 	const networkData = networksData.find(({ elements }: { elements: [] }) =>
 		elements.find(({ id: elementId }: { id: string }) => elementId === id)
 	);
-
 	const element = networkData.elements.find(
 		({ id: elementId }: { id: string }) => elementId === id
 	);
 
-	if (networkData) {
-		const { variableName } = element;
+	const { variableName, type } = element;
 
-		if (variableName !== variable) {
+	function handleInputBlur(e: React.ChangeEvent<HTMLInputElement>) {
+		if (e.target.value === "") {
+			console.log(e.target.value);
+			setVariable("???");
+		}
+	}
+
+	function handleInputClick(e: React.MouseEvent<HTMLInputElement>) {
+		const eventTarget = e.target as HTMLInputElement;
+		if (eventTarget.value === "???") {
+			setVariable("");
+		}
+	}
+
+	useEffect(() => {
+		const { id: networkId } = networkData;
+		modifyElement({
+			networkId,
+			id,
+			variableName: debouncedVariable,
+		});
+	}, [debouncedVariable]);
+
+	if (networkData) {
+		if (
+			variableName &&
+			type !== ElementType.Wire &&
+			variableName !== variable &&
+			variable &&
+			!debouncedVariable
+		) {
 			setVariable(variableName);
 		}
 	}
 
-	const { type } = element;
-
 	return (
 		<div
 			className={styles.elementWrapper}
-			onClick={type !== ElementType.Wire ? onClick : undefined}
 			style={{ cursor: type !== ElementType.Wire ? "pointer" : "auto" }}
 		>
-			<div className={styles.variable}>{variable}</div>
-			<ElementRenderer element={element} />
+			{type !== ElementType.Wire && (
+				<input
+					className={styles.variable}
+					type='text'
+					onClick={(e) => handleInputClick(e)}
+					onBlur={(e) => handleInputBlur(e)}
+					onChange={(e) => setVariable(e.target.value)}
+					value={variable}
+				/>
+			)}
+			<div onClick={type !== ElementType.Wire ? onClick : undefined}>
+				<ElementRenderer element={element} />
+			</div>
 		</div>
 	);
 }
